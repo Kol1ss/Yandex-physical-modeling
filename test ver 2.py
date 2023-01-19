d88451258f53bedd0e5973dae1973532f2871947
Kol1ss <122973509+Kol1ss@users.noreply.github.com>
2023-01-19 21:02:49 +1000

    Update test ver 2.py

--- test ver 2.py
+++ test ver 2.py
@@ -2,10 +2,14 @@ dt = 0.05  # Шаг по времени
 ax, ay = 0, -9.81  # Ускорения вдоль осей
 x, y = 0, 0  # Начальные координаты
 vx, vy = 10, 10  # Начальные скорости
-
+coords = []
+velocities = []
+coords_arr = []
+velocities_arr = []
 
 def update_coordinates():
-    global x, y, vx, vy
+    global x, y, vx, vy, coords_arr, velocities_arr
+
     while y >= 0:
         # Обновляем скорости
         vx += ax * dt
@@ -17,5 +21,7 @@ def update_coordinates():
         coords = [round(x, 3), round(y, 3)]
         velocities = [vx, vy]
         print(coords, '  ---  ', velocities) #решил красиво вывести
+        coords_arr.append(coords)
+        velocities_arr.append(coords)
 
 update_coordinates()
